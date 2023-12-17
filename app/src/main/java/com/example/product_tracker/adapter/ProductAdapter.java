63e65ddafe0d939debf8cccbf645966b1bd067d4
Leonardo <leocruciani@gmail.com>
2023-12-17 19:55:04 +0100

    BOH

--- app/src/main/java/com/example/product_tracker/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/product_tracker/adapter/ProductAdapter.java
@@ -1,4 +1,38 @@
 package com.example.product_tracker.adapter;
 
-public class ProductAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.product_tracker.model.Product;
+import com.example.producttracker.R;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+
+    private final List<Product> productList;
+    private final Context context;
+
+    public ProductAdapter(List<Product> productList, Context context) {
+        this.productList = productList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductAdapter.ViewHolder holder, int position) {
+
+    }
+
 }
